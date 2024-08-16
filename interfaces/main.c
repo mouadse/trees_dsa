67e50094db9df70a9ba7ac6688342c0e45e27d71
Mouad <mouadse@gmail.com>
2024-08-16 02:26:38 +0100

    X
    X

---
+++ interfaces/main.c
@@ -0,0 +1,6 @@
+#include "tree.h"
+
+int main(void) {
+  // code goes here
+  return (EXIT_SUCCESS);
+}
--- interfaces/tree.h
+++ interfaces/tree.h
@@ -1,4 +1,21 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct s_node {
+  int data;
+  struct s_node *leftChild;
+  struct s_node *rightChild;
+} node;
+
+typedef struct s_tree {
+  node *tree;
+  int (*compare)(const void *key1, const void *key2);
+  void (*destroy)(void *data);
+} b_tree;
+
 #endif
