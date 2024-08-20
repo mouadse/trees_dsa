7f09731c488ec0d91664f4cf497abd3ef7f9280a
Mouad <mouadse@gmail.com>
2024-08-20 23:20:40 +0100

    X
    X

--- udemy_course/main.c
+++ udemy_course/main.c
@@ -10,6 +10,49 @@ typedef struct t_node {
 
 node *root = NULL;
 
+void Insert(int data) {
+  node *t = root;
+  node *r;
+  node *p;
+
+  if (root == NULL) {
+    p = malloc(sizeof(node));
+    p->data = data;
+    p->lchild = p->rchild = NULL;
+    root = p;
+    return;
+  }
+  while (t != NULL) {
+    r = t;
+    if (data < t->data)
+      t = t->lchild;
+    else if (data > t->data)
+      t = t->rchild;
+    else {
+      return;
+    }
+  }
+  p = malloc(sizeof(node));
+
+  p->data = data;
+  p->lchild = p->rchild = NULL;
+
+  if (data < r->data)
+    r->lchild = p;
+  else {
+    r->rchild = p;
+  }
+}
+
+void Inorder(node *p) {
+  if (p != NULL) {
+    // left -> root -> right
+    Inorder(p->lchild);
+    printf("%d\n", p->data);
+    Inorder(p->rchild);
+  }
+}
+
 int main(void) {
   // code it here
   return (EXIT_SUCCESS);
